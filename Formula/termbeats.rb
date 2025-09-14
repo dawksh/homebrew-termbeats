461e57b06a6c4405eeec27fa36e9ac982570702c
daksh <akshd@outlook.com>
2025-09-14 23:30:31 +0530

    chore: update

--- Formula/termbeats.rb
+++ Formula/termbeats.rb
@@ -1,16 +1,18 @@
 class Termbeats < Formula
     desc "termbeats — make music on your terminal"
     homepage "https://github.com/dawksh/termbeats"
-    version "0.0.1"  # change this when you make new releases
-    url "https://github.com/dawksh/termbeats/archive/refs/tags/prebuild.tar.gz"
-    sha256 "cfd40e6baa831538cf97051f5a5fce609c89141ad1f219de98cbc1e1dcf9f88f"
+    version "0.0.1"
+    url "https://github.com/dawksh/termbeats/releases/download/v0.0.1/termbeats-macos-arm64.tar.gz"
+    sha256 "3792846b7c4b414939d69f21f8b640ffb0c7fedd63453b78b32746b892c9807f"
   
-    def install
-      bin.install "termbeats"
-    end
-  
-    test do
-      system "#{bin}/termbeats", "--version"
+    on_macos do
+      if Hardware::CPU.arm?
+        def install
+          bin.install "termbeats"
+        end
+      else
+        odie "termbeats only supports Apple Silicon (arm64) Macs"
+      end
     end
   end
   
