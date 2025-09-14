feb0f115b3b01e1318b4a3eaa2dcd1038b5b6fec
daksh <akshd@outlook.com>
2025-09-15 02:00:46 +0530

    chore: bump version

--- Formula/termbeats.rb
+++ Formula/termbeats.rb
@@ -1,7 +1,7 @@
 class Termbeats < Formula
     desc "termbeats — make music on your terminal"
     homepage "https://github.com/dawksh/termbeats"
-    version "0.0.1"
+    version "0.0.2"
     url "https://github.com/dawksh/termbeats/releases/download/v0.0.2/termbeats-macos-arm64.tar.gz"
     sha256 "77eef42f31fd8998a27490640ad691fcb0ffa4451e6598a47958750269efb948"
   
